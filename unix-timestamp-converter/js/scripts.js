0d29d5cb99727b10aad3a1daff5318adcb75fd90
Cory <cory.noonan@gmail.com>
2017-07-26 20:37:00 -0400

    heavy refactoring

--- unix-timestamp-converter/js/scripts.js
+++ unix-timestamp-converter/js/scripts.js
@@ -1,65 +1,51 @@
 require('datejs')
-const jstz = require('jstz')
+const { determine } = require('jstz')
 const $ = require('jQuery')
 const moment = require('moment')
-// function that takes the current timestamp and increments it by 1 second to keep the current time
+
+const timezoneName = determine().name();
+const [cleanDateElement] = document.getElementsByClassName("cleanDate")
+const [convertedDateUtcElement] = document.getElementsByClassName("convertedDateUtc")
+const [convertedDateUnixElement] = document.getElementsByClassName("convertedDateUnix")
+
 setInterval(function () {
   var unixCurrentTime = moment().unix();
   $("input[name='number']").attr("placeholder", "Unix Timestamp eg: " + unixCurrentTime).html(++unixCurrentTime);
 }, 1000);
 
-//Get the current timezone from jstimezonedetect
-var tz = jstz.determine();
-
-
-//Display your actual timezone
-$(".yourTz").html("<strong>" + tz.name() + ": " + "</strong>");
-
-//reset the form to the placeholders
-$("#convertDateForm").on("reset", function (e) {
-  if ($(".cleanDate").html() !== 'Date will display here') {
-    $(".yourTz").html("<strong>" + tz.name() + ": " + "</strong>");
-    $(".cleanDate").hide().fadeIn().html('Date will display here');
-    $(".convertedDateUtc").hide().fadeIn().html('Date will display here');
-    $(".convertedDateUnix").hide().fadeIn().html('Timestamp will display here');
-    $("#convertDateForm")[0].reset();
-    $("#MMDDYYYY").attr("class", "form-group");
-    $("#unixTimeInput").attr("class", "form-group");
-  }
-});
-
-
-//function to change
-$("#convertDateForm").on("submit", function (e) {
+const submit = (e) => {
   e.preventDefault();
-
   var unixTimestampInput = $("input[name='number']").val();
   var humanDate = $("input[name='fulldate']").val();
-  var d1 = Date.parse(humanDate);
 
-  if (unixTimestampInput === "" && d1 === null) {
+  if (unixTimestampInput === "" && humanDate === '') {
     unixTimestampInput = moment().unix();
   } else {
     unixTimestampInput = $("input[name='number']").val();
   }
-
   //if the unix timestamp input is not equal to an empty string run these functions that convert the input
   if (unixTimestampInput !== "") {
-    $(".cleanDate").html(cleanDate(unixTimestampInput));
-    $(".convertedDateUtc").html(convertedDateUtc(unixTimestampInput));
-    $(".convertedDateUnix").html(unixTimestampInput);
+    cleanDateElement.innerHTML = cleanDate(unixTimestampInput)
+    convertedDateUtcElement.innerHTML = convertedDateUtc(unixTimestampInput)
+    convertedDateUnixElement.innerHTML = unixTimestampInput;
   }
-
   // if the unix timestamp input is equal to an empty string run the function to convert the human readable date into the unix timestamp
   if (unixTimestampInput === "") {
-    $(".cleanDate").html(cleanDate(unixTime(d1)));
-    $(".convertedDateUtc").html(convertedDateUtc(unixTime(d1)));
-    $(".convertedDateUnix").html(unixTime(d1));
+    cleanDateElement.innerHTML = cleanDate(unixTime(humanDate))
+    convertedDateUtcElement.innerHTML = convertedDateUtc(unixTime(humanDate))
+    convertedDateUnixElement.innerHTML = unixTime(humanDate)
   }
+}
 
-});
+const reset = () => {
+  if (cleanDateElement.innerHTML !== 'Date will display here') {
+    cleanDateElement.innerHTML = 'Date will display here'
+    convertedDateUtcElement.innerHTML = 'Date will display here'
+    convertedDateUnixElement.innerHTML = 'Timestamp will display here'
+  }
+}
 
-//takes in a unix timestamp and creates a human readable date in your timezone
+
 function cleanDate(UNIX_timestamp) {
   var a = new Date(UNIX_timestamp * 1000);
   var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
@@ -90,8 +76,8 @@ function convertedDateUtc(UNIX_timestamp) {
 
 
 //takes the human date in and outputs the unix timestamp
-function unixTime(d1) {
-  var date = new Date(d1).getTime() / 1000;
+function unixTime(humanDate) {
+  var date = new Date(Date.parse(humanDate)).getTime() / 1000;
   return Math.round(date);
 }
 
@@ -101,3 +87,7 @@ module.exports = {
   convertedDateUtc: convertedDateUtc,
   cleanDate: cleanDate
 }
+
+document.getElementById('submit').onclick = submit
+document.getElementById('reset').onclick = reset
+document.getElementById("yourTz").innerHTML = `${timezoneName}: `
