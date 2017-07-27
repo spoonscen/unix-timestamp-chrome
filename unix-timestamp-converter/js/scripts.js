298233ced374ab25812d05d11922f8caeb84bcee
Cory <cory.noonan@gmail.com>
2017-07-26 21:57:10 -0400

    remove jquery dependency

--- unix-timestamp-converter/js/scripts.js
+++ unix-timestamp-converter/js/scripts.js
@@ -1,71 +1,59 @@
 require('datejs')
 const { determine } = require('jstz')
-const $ = require('jQuery')
 const moment = require('moment')
 
-const timezoneName = determine().name();
+const timezoneName = determine().name()
 const [cleanDateElement] = document.getElementsByClassName("cleanDate")
 const [convertedDateUtcElement] = document.getElementsByClassName("convertedDateUtc")
 const [convertedDateUnixElement] = document.getElementsByClassName("convertedDateUnix")
+const unixTimestampInput = document.getElementById('unixTimestampInput')
+const humanDateInput = document.getElementById('fulldate')
+const oneSecond = 1000
+const setPlaceholderText = () => unixTimestampInput.placeholder = `Unix Timestamp eg: ${moment().unix()}`
 
-setInterval(function () {
-  var unixCurrentTime = moment().unix();
-  $("input[name='number']").attr("placeholder", "Unix Timestamp eg: " + unixCurrentTime).html(++unixCurrentTime);
-}, 1000);
+setPlaceholderText()
+setInterval(setPlaceholderText, oneSecond)
 
 const submit = (e) => {
-  e.preventDefault();
-  var unixTimestampInput = $("input[name='number']").val();
-  var humanDate = $("input[name='fulldate']").val();
+  e.preventDefault()
+  const humanDateValue = humanDateInput.value
+  const bothInputsEmpty = unixTimestampInput.value === '' && humanDateValue === ''
+  const unixTimestampInputValue = bothInputsEmpty ? moment().unix() : unixTimestampInput.value
 
-  if (unixTimestampInput === "" && humanDate === '') {
-    unixTimestampInput = moment().unix();
-  } else {
-    unixTimestampInput = $("input[name='number']").val();
-  }
-  //if the unix timestamp input is not equal to an empty string run these functions that convert the input
-  if (unixTimestampInput !== "") {
-    cleanDateElement.innerHTML = cleanDate(unixTimestampInput)
-    convertedDateUtcElement.innerHTML = convertedDateUtc(unixTimestampInput)
-    convertedDateUnixElement.innerHTML = unixTimestampInput;
-  }
-  // if the unix timestamp input is equal to an empty string run the function to convert the human readable date into the unix timestamp
-  if (unixTimestampInput === "") {
-    cleanDateElement.innerHTML = cleanDate(unixTime(humanDate))
-    convertedDateUtcElement.innerHTML = convertedDateUtc(unixTime(humanDate))
-    convertedDateUnixElement.innerHTML = unixTime(humanDate)
+  cleanDateElement.innerHTML = cleanDate(unixTimestampInputValue)
+  convertedDateUtcElement.innerHTML = convertedDateUtc(unixTimestampInputValue)
+  convertedDateUnixElement.innerHTML = unixTimestampInputValue
+
+  if (humanDateValue) {
+    cleanDateElement.innerHTML = cleanDate(getUnixTimestamp(humanDateValue))
+    convertedDateUtcElement.innerHTML = convertedDateUtc(getUnixTimestamp(humanDateValue))
+    convertedDateUnixElement.innerHTML = getUnixTimestamp(humanDateValue)
   }
 }
 
 const reset = () => {
-  if (cleanDateElement.innerHTML !== 'Date will display here') {
-    cleanDateElement.innerHTML = 'Date will display here'
-    convertedDateUtcElement.innerHTML = 'Date will display here'
-    convertedDateUnixElement.innerHTML = 'Timestamp will display here'
-  }
+  const defaultText = 'Date will display here'
+  if (cleanDateElement.innerHTML === defaultText) return
+  cleanDateElement.innerHTML = defaultText
+  convertedDateUtcElement.innerHTML = defaultText
+  convertedDateUnixElement.innerHTML = 'Timestamp will display here'
 }
 
-
-function cleanDate(UNIX_timestamp) {
-  return moment(new Date(UNIX_timestamp * 1000)).format('MMMM Do YYYY, HH:mm:ss a');
+function cleanDate(timestamp) {
+  return moment(new Date(timestamp * 1000)).format('MMMM Do YYYY, HH:mm:ss a')
 }
 
-
-//takes a unix timestamp in and creates a human readable date in UTC
-function convertedDateUtc(UNIX_timestamp) {
-  return moment.utc(new Date(UNIX_timestamp * 1000)).format('MMMM Do YYYY, HH:mm:ss a');
+function convertedDateUtc(timestamp) {
+  return moment.utc(new Date(timestamp * 1000)).format('MMMM Do YYYY, HH:mm:ss a')
 }
 
-
-//takes the human date in and outputs the unix timestamp
-function unixTime(humanDate) {
-  var date = new Date(Date.parse(humanDate)).getTime() / 1000;
-  return Math.round(date);
+function getUnixTimestamp(humanDate) {
+  const date = new Date(Date.parse(humanDate)).getTime() / 1000
+  return Math.round(date)
 }
-
 
 module.exports = {
-  unixTime: unixTime,
+  getUnixTimestamp: getUnixTimestamp,
   convertedDateUtc: convertedDateUtc,
   cleanDate: cleanDate
 }
