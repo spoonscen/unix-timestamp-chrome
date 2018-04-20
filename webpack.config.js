226088d11f7b64311a5822c376273c002d278ff4
Cory Noonan <cory.noonan@gmail.com>
2018-04-19 22:27:47 -0400

    udpates moment

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require('webpack')
+const path = require('path')
 
-const BUILD_DIR = path.resolve(__dirname, 'public');
-const APP_DIR = path.resolve(__dirname, 'src/js');
+const BUILD_DIR = path.resolve(__dirname, 'public')
+const APP_DIR = path.resolve(__dirname, 'src/js')
 
 const config = {
   entry: APP_DIR + '/index.jsx',
@@ -27,8 +27,8 @@ const config = {
     extensions: [
       '.js',
       '.jsx'
-  ]
+    ]
+  }
 }
-};
 
-module.exports = config;
+module.exports = config
